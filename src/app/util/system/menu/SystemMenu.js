4fe74e92b1d1b78d562fab9b3751bb1cf5829eb4
Florian Cargoët <florian.cargoet@gmail.com>
2013-11-23 13:57:00 +0100

    minor refactor: easier to read file menu creation

--- src/app/util/system/menu/SystemMenu.js
+++ src/app/util/system/menu/SystemMenu.js
@@ -45,19 +45,7 @@ Ext.define('AS.util.system.menu.SystemMenu', {
   initFileMenu: function() {
 
     // Create file menu
-    this.fileMenu = new gui.Menu();
-
-    // New file
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Nouveau fichier',
-      click: function(){},
-    }));
-
-    // Open file
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Ouvrir...',
-      click: function(){}
-    }));
+    var fileMenu = this.fileMenu = new gui.Menu();
 
     // Open recent menu
     this.openRecentMenu = new gui.Menu();
@@ -66,52 +54,66 @@ Ext.define('AS.util.system.menu.SystemMenu', {
       click: function(){}
     }));
     this.openRecentMenu.append(new gui.MenuItem({ type: 'separator' }));
-    this.fileMenu.append(new gui.MenuItem({ label: 'Ouvrir récent', submenu: this.openRecentMenu }));
 
-    // Open folder
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Ouvrir dossier...',
-      click: function() {}
-    }));
 
-    // Separator
-    this.fileMenu.append(new gui.MenuItem({ type: 'separator' }));
+    var items = [
+      // New file
+      {
+        label: 'Nouveau fichier',
+        click: function(){},
+      },
+      // Open file
+      {
+        label: 'Ouvrir...',
+        click: function(){}
+      },
+      // Open recent menu
+      {
+        label: 'Ouvrir récent',
+        submenu: this.openRecentMenu
+      },
+      // Open folder
+      {
+        label: 'Ouvrir dossier...',
+        click: function() {}
+      },
+      // Separator
+      { type: 'separator' },
+      // Save file
+      {
+        label: 'Enregistrer',
+        click: function() {}
+      },
+      // Save file as
+      {
+        label: 'Enregistrer sous...',
+        click: function(){}
+      },
+      // Save all files
+      {
+        label: 'Enregistrer tout les fichiers',
+        click: function() {}
+      },
+      // Separator
+      { type: 'separator' },
+      // Close file
+      {
+        label: 'Fermer le fichier',
+        click: function() {}
+      },
+      // Close all files
+      {
+        label: 'Fermer tous les fichiers',
+        click: function() {}
+      }
+    ];
 
-    // Save file
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Enregistrer',
-      click: function() {}
-    }));
-
-    // Save file as
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Enregistrer sous...',
-      click: function(){}
-    }));
-
-    // Save all files
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Enregistrer tout les fichiers',
-      click: function() {}
-    }));
-
-    // Separator
-    this.fileMenu.append(new gui.MenuItem({ type: 'separator' }));
-
-    // Close file
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Fermer le fichier',
-      click: function() {}
-    }));
-
-    // Close all files
-    this.fileMenu.append(new gui.MenuItem({
-      label: 'Fermer tous les fichiers',
-      click: function() {}
-    }));
+    items.forEach(function (item) {
+      fileMenu.append(new gui.MenuItem(item));
+    });
 
     // Insert file menu in System Menu
-    this.systemMenu.insert(new gui.MenuItem({ label: 'Fichier', submenu: this.fileMenu}), 1);
+    this.systemMenu.insert(new gui.MenuItem({ label: 'Fichier', submenu: fileMenu}), 1);
   }
 
 });
